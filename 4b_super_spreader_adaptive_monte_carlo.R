beae00df0692d14d84d56b468833861a15a223b9
hanmacrad2 <craddoch@tcd.ie>
2021-06-15 18:38:36 +0100

    MCMC for superspreader model
    MCMC for superspreader model

--- 4b_super_spreader_adaptive_monte_carlo.R
+++ 4b_super_spreader_adaptive_monte_carlo.R
@@ -3,10 +3,11 @@
 library(gridExtra)
 
 #Setup
-source("1_simulation.R")
+source("1b_simulation_super_spreading_events.R")
 par(mar=c(1,1,1,1))
 
 #Params
+n = 50000
 num_days = 60 #100
 shape_gamma = 6
 scale_gamma = 1
@@ -32,7 +33,7 @@ log_like_ss <- function(x, r01, r02, p){
     
     lambda1 = r01*sum(x[1:t-1]*rev(prob_infect[1:t-1]))
     lambda2 = r02*sum(x[1:t-1]*rev(prob_infect[1:t-1]))
-    logl = logl + log(p) + log(lambda1) + log(1-p) + log(lamda2) - lambda1 - lambda2
+    logl = logl + log(p) + log(lambda1) + log(1-p) + log(lambda2) - lambda1 - lambda2
     
   }
   
@@ -43,7 +44,7 @@ log_like_ss <- function(x, r01, r02, p){
 
 #********************************************************************
 #Adaptive MCMC
-adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in = 2500
+adaptive_mc_r0_ss <- function(data, n, sigma1, sigma2, sigma3, x0 = 1, burn_in = 5000) { #burn_in = 2500
   
   'Returns mcmc samples of R0 & acceptance rate'
   
@@ -57,8 +58,12 @@ adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in
   p_vec[1] <- x0
   
   U <- runif(n)
-  count_accept = 0
-  count_reject = 0
+  count_accept1 = 0
+  count_reject1 = 0
+  count_accept2 = 0
+  count_reject2 = 0
+  count_accept3 = 0
+  count_reject3 = 0
   sd_sample = 1
   
   #MCMC chain
@@ -67,11 +72,14 @@ adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in
     #******************
     #ro1
     ro1_dash <- r01_vec[i-1] + rnorm(1, sd = sigma1) 
-    if(Y < 0){
-      Y = abs(Y)
+    if(ro1_dash < 0){
+      ro1_dash = abs(ro1_dash)
     }
     
-    log_alpha = log_like_ss(data, ro1_dash, r02_vec[i-1], p_vec[i-1]) - log_like_ss(data, r01_vec[i-1], r02_vec[i-1], p_vec[i-1]) + dgamma(ro1_dash, shape = 1, scale = 1, log = TRUE) - dgamma(r01_vec[i-1], shape = 1, scale = 1, log = TRUE) #log_prior(theta_dash) - log_prior(theta) = 1 - 1 
+    log_alpha = log_like_ss(data, ro1_dash, r02_vec[i-1], p_vec[i-1])
+    - log_like_ss(data, r01_vec[i-1], r02_vec[i-1], p_vec[i-1])
+    + dgamma(ro1_dash, shape = 1, scale = 1, log = TRUE)
+    - dgamma(r01_vec[i-1], shape = 1, scale = 1, log = TRUE) 
     
     if(!(is.na(log_alpha)) && log(U[i]) < log_alpha) {
       r01_vec[i] <- ro1_dash
@@ -93,7 +101,9 @@ adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in
       ro2_dash = abs(ro2_dash)
     }
     
-    log_alpha = log_like_ss(data, r01_vec[i], ro2_dash, p_vec[i-1]) - log_like_ss(data, r01_vec[i], r02_vec[i-1], p_vec[i-1]) + dgamma(ro1_dash, shape = 1, scale = 1, log = TRUE) - dgamma(r01_vec[i-1], shape = 1, scale = 1, log = TRUE)  
+    log_alpha = log_like_ss(data, r01_vec[i], ro2_dash, p_vec[i-1]) - log_like_ss(data, r01_vec[i], r02_vec[i-1], p_vec[i-1])
+    + dgamma(ro2_dash, shape = 1, scale = 1, log = TRUE)
+    - dgamma(r02_vec[i-1], shape = 1, scale = 1, log = TRUE) #Do other priors cancel?
     
     
     if(!(is.na(log_alpha)) && log(U[i]) < log_alpha) {
@@ -116,14 +126,17 @@ adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in
       p_dash = abs(p_dash)
     }
     
-    log_alpha = log_like(data, r01_vec[i], r02_vec[i-1], p_dash) - log_like(data, r01_vec[i], r02_vec[i-1], p_vec[i-1]) + dgamma(p_dash, shape = 1, scale = 1, log = TRUE) - dgamma(p_vec[i-1], shape = 1, scale = 1, log = TRUE) 
+    log_alpha = log_like(data, r01_vec[i], r02_vec[i-1], p_dash) 
+    - log_like(data, r01_vec[i], r02_vec[i-1], p_vec[i-1])
+    + dgamma(p_dash, shape = 1, scale = 1, log = TRUE)
+    - dgamma(p_vec[i-1], shape = 1, scale = 1, log = TRUE) 
     
     if(!(is.na(log_alpha)) && log(U[i]) < log_alpha) {
       p_vec[i] <- p_dash
       count_accept3 = count_accept3 + 1
     } else {
       p_vec[i] <- p_vec[i-1]
-      count_reject2 = count_reject2 + 1
+      count_reject3 = count_reject3 + 1
     }
     
     #Adaptive MC
@@ -148,19 +161,19 @@ adaptive_mc_r0_ss <- function(data, n, sigma, x0 = 1, burn_in = 5000) { #burn_in
 }
 
 #Plots
-mcmc_plotting_adaptive <- function(mcmc_vector, r0_true, folder_dir_ad) {
+mcmc_plotting_adaptive_ss <- function(mcmc_vector1, mcmc_vector2, mcmc_vector3, r0_true, folder_dir_ad) {
   
   #Folder save
-  pdf(paste(folder_dir_ad, "/", "adpative_mc_r0_true_", r0_true, ".pdf", sep=""))
+  pdf(paste(folder_dir_ad, "/", "ss_adpative_mc_r0_true_", r0_true, ".pdf", sep=""))
   
   #i. MCMC chain
-  plot1 = ts.plot(mcmc_vector, ylab = 'R0', main = paste("Adaptive MC for R0, true R0 = ", r0_true))
+  plot1 = ts.plot(mcmc_vector1, ylab = 'R0', main = paste("Superspreader Adaptive MC for R01, true R0 = ", r0_true))
   print(plot1)
   
   #ii. Mean
   #Plot mean
-  r0_mean = cumsum(mcmc_vector)/seq_along(mcmc_vector)
-  plot2 = plot(seq_along(r0_mean), r0_mean, xlab = 'Time', ylab = 'R0', main = paste("Mean of R0 MCMC chain, True R0 = ",r0_true))
+  r0_mean = cumsum(mcmc_vector1)/seq_along(mcmc_vector1)
+  plot2 = plot(seq_along(r0_mean), r0_mean, xlab = 'Time', ylab = 'R01', main = paste("Mean of R01 MCMC chain, True R0 = ",r0_true))
   print(plot2)
   
   #Histogram
@@ -171,8 +184,55 @@ mcmc_plotting_adaptive <- function(mcmc_vector, r0_true, folder_dir_ad) {
   hist2 <- hist(mcmc_vector, breaks = 80)
   hist2$counts <- hist2$counts/sum(hist2$counts)
   hist3 = plot(hist2, xlab = 'r0', ylab = 'Density', 
+               main = 'Empirical density of r01 - MCMC chain')
+  print(hist3)
+  
+  #2. ro2
+  #**************************************************
+  #i. MCMC chain
+  plot1 = ts.plot(mcmc_vector2, ylab = 'R02', main = paste("Superspreader Adaptive MC for R02, true R0 = ", r0_true))
+  print(plot1)
+  
+  #ii. Mean
+  #Plot mean
+  r0_mean = cumsum(mcmc_vector2)/seq_along(mcmc_vector2)
+  plot2 = plot(seq_along(r0_mean), r0_mean, xlab = 'Time', ylab = 'R02', main = paste("Mean of R02 MCMC chain, True R0 = ",r0_true))
+  print(plot2)
+  
+  #Histogram
+  #hist1 = hist(mcmc_vector, prob = TRUE)
+  #print(hist1)
+  
+  #Hist
+  hist2 <- hist(mcmc_vector2, breaks = 80)
+  hist2$counts <- hist2$counts/sum(hist2$counts)
+  hist3 = plot(hist2, xlab = 'r0', ylab = 'Density', 
+               main = 'Empirical density of r02 - MCMC chain')
+  print(hist3)
+  
+  #**************************************************
+  #*p
+  #i. MCMC chain
+  plot1 = ts.plot(mcmc_vector3, ylab = 'p', main = paste("Superspreader Adaptive MC for p, true R0 = ", r0_true))
+  print(plot1)
+  
+  #ii. Mean
+  #Plot mean
+  r0_mean = cumsum(mcmc_vector3)/seq_along(mcmc_vector3)
+  plot2 = plot(seq_along(r0_mean), r0_mean, xlab = 'Time', ylab = 'p', main = paste("Mean of p MCMC chain, True R0 = ",r0_true))
+  print(plot2)
+  
+  #Histogram
+  #hist1 = hist(mcmc_vector, prob = TRUE)
+  #print(hist1)
+  
+  #Hist
+  hist2 <- hist(mcmc_vector3, breaks = 80)
+  hist2$counts <- hist2$counts/sum(hist2$counts)
+  hist3 = plot(hist2, xlab = 'r0', ylab = 'Density', 
                main = 'Empirical density of r0 - MCMC chain')
   print(hist3)
+  
   dev.off()
   
 }
@@ -189,7 +249,7 @@ mcmc_plotting_adaptive <- function(mcmc_vector, r0_true, folder_dir_ad) {
 #********************************************************************
 #Apply Adaptive MCMC to a range of r0s
 
-apply_adaptive_mc_range_r0 <- function(list_r0, sigma, folder_dir_ad){
+apply_adaptive_mc_range_r0_ss <- function(list_r0, sigma1, sigma2, sigma3, prop_ss, magnitude_ss, folder_dir_ad){
   
   #Create folder
   ifelse(!dir.exists(file.path(folder_dir_ad)), dir.create(file.path(folder_dir_ad)), FALSE)
@@ -202,34 +262,40 @@ apply_adaptive_mc_range_r0 <- function(list_r0, sigma, folder_dir_ad){
   for (r0X in list_r0){
     
     #Get simulated data when r0 is r0X
-    data = simulate_branching(num_days, r0X, shape_gamma, scale_gamma)
+    data = simulate_branching_ss(num_days, r0X, shape_gamma, scale_gamma, prop_ss, magnitude_ss)
     
     #Time
     start_time = Sys.time()
-    mcmc_params_ad = adaptive_mc_r0(data, n, sigma)
+    mcmc_params_ad = adaptive_mc_r0_ss(data, n, sigma1, sigma2, sigma3)
     end_time = Sys.time()
     time_elap = end_time - start_time
     print('Time elapsed:')
     print(time_elap)
     
     #Extract params
-    r0_mcmc = mcmc_params_ad[1]
+    r01_mcmc = mcmc_params_ad[1]
     r0_mcmc = unlist(r0_mcmc)
     
-    accept_rate = mcmc_params_ad[[2]]
+    r02_mcmc = mcmc_params_ad[2]
+    r02_mcmc = unlist(r02_mcmc)
+    
+    p_mcmc = mcmc_params_ad[3]
+    p_mcmc = unlist(p_mcmc)
+    
+    accept_rate = mcmc_params_ad[[4]]
     list_accept_rate[i] = round(accept_rate, 2)
     
-    num_samples = mcmc_params_ad[[3]]
+    num_samples = mcmc_params_ad[[5]]
     list_num_samp[i] = num_samples
     
-    sd_final = mcmc_params_ad[[4]]
+    sd_final = mcmc_params_ad[[6]]
     list_sd[i] = round(sd_final, 3)
     
     list_time_taken[i] = round(time_elap, 2)
     i = i + 1
     
     #Apply Plotting
-    mcmc_plotting_adaptive(r0_mcmc, r0X, folder_dir_ad)
+    mcmc_plotting_adaptive_ss(r01_mcmc, r02_mcmc, p_mcmc, r0X, folder_dir_ad)
     
   }
   
@@ -247,9 +313,11 @@ apply_adaptive_mc_range_r0 <- function(list_r0, sigma, folder_dir_ad){
 }
 
 #Apply
+prop_ss = 0.1
+magnitude_ss = 10
 sigma = 0.75
-folder_dir_ad = 'Results/Adaptive_MC/adaptive_mc_iter_VII_burn_in_5k'
+folder_dir_ad = 'Results/super_spreaders/ss_model_I_burn_in_5k'
 list_r0 = c(0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3, 3.5, 4.0, 4.5, 5.0, 8.0, 10.0)  #c(0.8, 0.9, 1.0, 2.75, 3, 3.5, 4.0, 4.5, 5.0, 8.0, 10.0) #c(0.8, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5,
 #list_r0 = c(0.5, 0.65, 0.70, 0.75, 0.8, 0.85, 0.95, 1.05, 2.80, 3.05, 3.55, 4.05, 4.55, 5.05, 8.05, 10.05)
-df_ad_results_formI = apply_adaptive_mc_range_r0(list_r0, sigma, folder_dir_ad)
+df_ss_results = apply_adaptive_mc_range_r0_ss(list_r0, sigma, sigma, sigma, prop_ss, magnitude_ss, folder_dir_ad)
 
