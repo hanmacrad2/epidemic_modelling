a8e7b077398b4d2a65be7f99c320c540e53cbbea
hanmacrad2 <craddoch@tcd.ie>
2021-07-27 11:10:44 +0100

    functions file
    functions file

---
+++ functions.R
@@ -0,0 +1,183 @@
+#Functions
+
+#*********************************************************
+#*Simulation Functions
+
+#Baseline simulation
+simulate_branching = function(num_days, r0, shape_gamma, scale_gamma) {
+  
+  'Baseline simulation model'
+  #Set up
+  vec_infecteds = vector('numeric', num_days)
+  vec_infecteds[1] = 2
+  
+  #Infectiousness (Discrete gamma) - I.e 'Infectiousness Pressure' - Sum of all people
+  #Explanation: Gamma is a continuous function so integrate over the density at that point in time (today - previous day)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  
+  #Days of Infection Spreading
+  for (t in 2:num_days) {
+    
+    #Total rate
+    tot_rate = r0*sum(vec_infecteds[1:(t-1)]*rev(prob_infect[1:(t-1)])) #Product of infecteds & their probablilty of infection along the gamma dist at that point in time
+    vec_infecteds[t] = rpois(1, tot_rate) #Assuming number of cases each day follows a poisson distribution. Causes jumps in data 
+  }
+  
+  vec_infecteds
+}
+
+#*******************************************************
+#Super-spreading simulation
+
+simulate_branching_ss = function(num_days, shape_gamma, scale_gamma, alphaX, betaX, gammaX) {
+  'Simulate an epidemic with Superspreading events
+  prop_ss = Proportion of superspreading days
+  magnitude_ss = increased rate of superspreading event'
+  
+  #Set up
+  total_infecteds = vector('numeric', num_days)
+  nsse_infecteds = vector('numeric', num_days)
+  sse_infecteds = vector('numeric', num_days)
+  total_infecteds[1] = 1
+  nsse_infecteds[1] = 1
+  sse_infecteds[1] = 0
+  
+  #Infectiousness (Discrete gamma) - I.e 'Infectiousness Pressure' - Sum of all people
+  #Explanation: Gamma is a continuous function so integrate over the density at that point in time (today - previous day)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  
+  #Days of Infection Spreading
+  for (t in 2:num_days) {
+    
+    #Regular infecteds (tot_rate = lambda) fix notation
+    lambda_t = sum(nsse_infecteds[1:(t-1)]*rev(prob_infect[1:(t-1)])) #?Why is it the reversed probability
+    tot_rate = alphaX*lambda_t #Product of infecteds & their probablilty of infection along the gamma dist at that point in time
+    nsse_infecteds[t] = rpois(1, tot_rate) #Assuming number of cases each day follows a poisson distribution. Causes jumps in data 
+    
+    #Super-spreaders
+    n_t = rpois(1, betaX*lambda_t) #Number of super-spreading events (beta)
+    
+    if (n_t > 0){
+      sse_infecteds[t] = rpois(1, gammaX*n_t) #z_t: Total infecteds due to super-spreading event - num of events x Num individuals
+    }
+    
+    total_infecteds[t] = nsse_infecteds[t] + sse_infecteds[t]
+  }
+  
+  total_infecteds
+}
+
+#*******************************************************************************
+#Model functions  - Super-spreading Model
+
+#Log Likelihood 
+log_like_ss <- function(x, alphaX, betaX, gammaX){
+  
+  #Params
+  num_days = length(x)
+  shape_gamma = 6
+  scale_gamma = 1
+  
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  logl = 0
+  
+  for (t in 2:num_days) {
+    
+    lambda_t = sum(x[1:(t-1)]*rev(prob_infect[1:(t-1)]))
+    inner_sum_xt = 0
+    
+    for (y_t in 0:x[t]){ #Sum for all values of y_t
+      
+      #Log likelihood
+      inner_sum_xt = (inner_sum_xt + exp(-alphaX*lambda_t)*(1/factorial(y_t))*(alphaX*lambda_t)^y_t*
+                        (gamma((x[t] - y_t) + betaX*lambda_t))/(gamma(betaX*lambda_t)*
+                                                                  factorial(x[t] - y_t))*(1/(gammaX +1))^(betaX*lambda_t)*
+                        (gammaX/(gammaX + 1))^(x[t] - y_t))
+      
+    } 
+    
+    logl = logl + log(inner_sum_xt) 
+    
+  }
+  
+  logl
+  
+}
+
+#*****************************************************
+#Log Likelihood - log-exp-sum trick 
+log_like_ss_lse <- function(x, alphaX, betaX, gammaX){
+  
+  #Params
+  num_days = length(x)
+  shape_gamma = 6
+  scale_gamma = 1
+  
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  logl = 0 
+  
+  for (t in 2:num_days) {
+    
+    #print(t)
+    lambda_t = sum(x[1:(t-1)]*rev(prob_infect[1:(t-1)]))
+    
+    if(x[t] == 0){ #y_t also equal to zero
+      
+      #L(x_i) for y_t, x_t = 0
+      logl = logl -(alphaX*lambda_t) - 
+        (betaX*lambda_t*log(gammaX +1))
+      
+    } else {
+      
+      #Terms in inner sum
+      inner_sum_vec <- vector('numeric', x[t])
+      
+      for (y_t in 0:x[t]){ #Sum for all values of y_t up to x_t
+        
+        #Store inner L(x_i) term in vector position
+        inner_sum_vec[y_t + 1] = (-(alphaX*lambda_t) - lfactorial(y_t) + y_t*log(alphaX*lambda_t) +
+                                    lgamma((x[t] - y_t) + (betaX*lambda_t)) - lgamma(betaX*lambda_t) - 
+                                    lfactorial(x[t] - y_t) - (betaX*lambda_t*log(gammaX +1)) + 
+                                    (x[t] - y_t)*log(gammaX) -(x[t] - y_t)*log(gammaX + 1))
+        
+      }
+      
+      #Calculate max element in inner vector, for all y_t for a given t, x[t]
+      lx_max = max(inner_sum_vec)
+      
+      #Calculate lse
+      lse = lx_max + log(sum(exp(inner_sum_vec - lx_max) ))
+      
+      #Add to overall log likelihood 
+      logl = logl + lse 
+      
+    }
+    
+  }
+  
+  logl
+  
+}
+
+
+#Apply
+#Parameters
+n = 50000
+num_days = 15 #60 #100
+shape_gamma = 6
+scale_gamma = 1
+#Priors
+prior_alpha_k = 1
+prior_alpha_theta = 1
+#x = simulate_branching_ss(num_days, shape_gamma, scale_gamma, alphaX, gammaX, betaX)
+
+#Log likelihood 
+#logl_1 = log_like_ss(x, alphaX, betaX, gammaX)
+#print(logl_1)
+
+#log exp sum trick
+#logl_2 = log_like_ss_lse(x, alphaX, betaX, gammaX)
+#print(logl_2)
+
--- miscellaneous/V1_4b_MCMC_SS.R
+++ miscellaneous/V1_4b_MCMC_SS.R
@@ -1,9 +1,10 @@
+
 #Description
-#Adaptive Monte Carlo targeting alpha, beta, gamma
+#Adaptive Monte Carlo targeting alpha
+library(gridExtra)
 
 #Setup
-setwd("~/GitHub/epidemic_modelling")
-source("functions.R")
+source("1b_simulation_super_spreading_events.R")
 #par(mar=c(1,1,1,1))
 
 #Params
@@ -14,6 +15,124 @@ scale_gamma = 1
 #Priors
 prior_alpha_k = 1
 prior_alpha_theta = 1
+
+
+#***********************************
+#Log Likelihood 
+log_like_ss <- function(x, alphaX, betaX, gammaX){
+  
+  #Params
+  num_days = length(x)
+  shape_gamma = 6
+  scale_gamma = 1
+  
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  logl = 0
+  
+  for (t in 2:num_days) {
+    
+    lambda_t = sum(x[1:t-1]*rev(prob_infect[1:t-1]))
+    inner_sum_xt = 0
+    
+    for (y_t in 0:x[t]){ #Sum for all values of y_t
+      
+      #Log likelihood
+      inner_sum_xt = inner_sum_xt + exp(-alphaX*lambda_t)*(1/factorial(y_t))*(alphaX*lambda_t)^y_t*
+        (gamma((x[t] - y_t) + betaX*lambda_t))/(gamma(betaX*lambda_t)*
+                                                  factorial(x[t] - y_t))*(1/(gammaX +1))^(betaX*lambda_t)*
+        (gammaX/(gammaX + 1))^(x[t] - y_t)
+      
+    } 
+    #print('x[t]')
+    #print(x[t])
+    logl = logl + log(inner_sum_xt) 
+    #print('inner_sum_xt')
+    #print(inner_sum_xt)
+    #print('log_inner_sum_xt')
+    #print(log(inner_sum_xt))
+    #print('logl')
+    #print(logl)
+  }
+  
+  logl
+  
+}
+
+#Apply
+num_days = 15
+x = simulate_branching_ss(num_days, shape_gamma, scale_gamma, alphaX, gammaX, betaX)
+x
+logl_1 = log_like_ss(x, alphaX, betaX, gammaX)
+logl_1
+
+#***********************************#*******************************************
+#Log Likelihood - log-exp-sum trick 
+log_like_ss_lse <- function(x, alphaX, betaX, gammaX){
+  
+  #Params
+  num_days = length(x)
+  shape_gamma = 6
+  scale_gamma = 1
+  
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  logl = 0.000001
+  
+  for (t in 2:num_days) {
+    
+    lambda_t = sum(x[1:t-1]*rev(prob_infect[1:t-1]))
+    inner_sum_vec <- vector('numeric', x[t])
+    #inner_sum_xt = 0
+    print('x[t]')
+    print(x[t])
+    
+    
+    for (y_t in 1:x[t]){ #Sum for all values of y_t
+      
+      #Store inner product in vector position
+      inner_sum_vec[y_t] = exp(-alphaX*lambda_t)*(1/factorial(y_t))*(alphaX*lambda_t)^y_t*
+        (gamma((x[t] - y_t) + betaX*lambda_t))/(gamma(betaX*lambda_t)*
+                                                  factorial(x[t] - y_t))*(1/(gammaX +1))^(betaX*lambda_t)*
+        (gammaX/(gammaX + 1))^(x[t] - y_t)
+      
+    }
+    #Calculate max of vector s vec_terms(which contains each terms) for a given y_t
+    print(inner_sum_vec)
+    x_max = max(inner_sum_vec)
+    print(x_max)
+    
+    #Calculate lse
+    innersum2 = 0
+    for (i in 1:length(inner_sum_vec)){
+      innersum2 = innersum2 + exp(inner_sum_vec[i] - x_max)
+    }
+    print('innersum2')
+    print(innersum2)
+    lse = x_max + log(innersum2)
+    #print('type of lse')
+    #print(typeof(lse))
+    print('lse')
+    print(lse)
+    #Add to overall log likelihood 
+    logl = logl + lse 
+    print('logl')
+    print(logl)
+    print(typeof(logl))
+    
+    
+  }
+  
+  logl
+  
+}
+
+#Apply
+#num_days = 10
+#x = simulate_branching_ss(num_days, shape_gamma, scale_gamma, alphaX, gammaX, betaX)
+#x
+logl_1 = log_like_ss_lse(x, alphaX, betaX, gammaX)
+logl_1
 
 #********************************************************************
 #Adaptive MCMC
@@ -49,8 +168,8 @@ adaptive_mc_ss <- function(data, n, sigma1, sigma2, sigma3, x0 = 1, burn_in = 50
       alpha_dash = abs(alpha_dash)
     }
     
-    log_alpha = log_like_ss_lse_lse(data, alpha_dash, beta_vec[i-1], gamma_vec[i-1])
-    - log_like_ss_lse(data, alpha_vec[i-1], beta_vec[i-1], gamma_vec[i-1])
+    log_alpha = log_like_ss(data, alpha_dash, beta_vec[i-1], gamma_vec[i-1])
+    - log_like_ss(data, alpha_vec[i-1], beta_vec[i-1], gamma_vec[i-1])
     + dgamma(alpha_dash, shape = 1, scale = 1, log = TRUE)
     - dgamma(alpha_vec[i-1], shape = 1, scale = 1, log = TRUE) 
     
@@ -75,8 +194,8 @@ adaptive_mc_ss <- function(data, n, sigma1, sigma2, sigma3, x0 = 1, burn_in = 50
       beta_dash = abs(beta_dash)
     }
     
-    log_alpha = log_like_ss_lse(data, alpha_vec[i], beta_dash, gamma_vec[i-1]) 
-    - log_like_ss_lse(data, alpha_vec[i], beta_vec[i-1], gamma_vec[i-1])
+    log_alpha = log_like_ss(data, alpha_vec[i], beta_dash, gamma_vec[i-1]) 
+    - log_like_ss(data, alpha_vec[i], beta_vec[i-1], gamma_vec[i-1])
     + dgamma(beta_dash, shape = 1, scale = 1, log = TRUE)
     - dgamma(beta_vec[i-1], shape = 1, scale = 1, log = TRUE) #Do other priors cancel?
     
@@ -101,8 +220,8 @@ adaptive_mc_ss <- function(data, n, sigma1, sigma2, sigma3, x0 = 1, burn_in = 50
       gamma_dash = abs(gamma_dash)
     }
     
-    log_alpha = log_like_ss_lse(data, alpha_vec[i], beta_vec[i-1], gamma_dash)  
-    - log_like_ss_lse(data, alpha_vec[i], beta_vec[i-1], gamma_vec[i-1])
+    log_alpha = log_like_ss(data, alpha_vec[i], beta_vec[i-1], gamma_dash)  
+    - log_like_ss(data, alpha_vec[i], beta_vec[i-1], gamma_vec[i-1])
     + dgamma(gamma_dash, shape = 1, scale = 1, log = TRUE)
     - dgamma(gamma_vec[i-1], shape = 1, scale = 1, log = TRUE) 
     
@@ -157,10 +276,8 @@ adaptive_mc_ss <- function(data, n, sigma1, sigma2, sigma3, x0 = 1, burn_in = 50
 
 #********
 #*Implement
+#*
 
-
-
-#**********************************************
 #Plots
 mcmc_plotting_adaptive_ss <- function(mcmc_vector1, mcmc_vector2, mcmc_vector3, alpha_true, folder_dir_ad) {
   
@@ -340,8 +457,8 @@ sigma = 0.75
 folder_dir_ad = 'Results/super_spreaders/ss_model_III_iterIII'
 list_alphaX = c(1.0, 2.0) #c(0.9, 1.25, 1.75, 2.0, 2.5, 3, 3.5, 4.0, 5.0, 8.0) #c(0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3, 3.5, 4.0, 4.5, 5.0, 8.0, 10.0)  #c(0.8, 0.9, 1.0, 2.75, 3, 3.5, 4.0, 4.5, 5.0, 8.0, 10.0) #c(0.8, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5,
 #list_alpha = c(0.5, 0.65, 0.70, 0.75, 0.8, 0.85, 0.95, 1.05, 2.80, 3.05, 3.55, 4.05, 4.55, 5.05, 8.05, 10.05)
-#df_ss_results = apply_adaptive_mc_range_alpha_ss(list_alphaX, sigma, sigma, sigma, betaX, gammaX, folder_dir_ad)
+df_ss_results = apply_adaptive_mc_range_alpha_ss(list_alphaX, sigma, sigma, sigma, betaX, gammaX, folder_dir_ad)
 
 
-
+#Checks
 
