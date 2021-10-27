7eeabfbf3e5c2538057d44afe9758dcf8db9f87b
hanmacrad2 <craddoch@tcd.ie>
2021-10-27 12:51:30 +0100

    MCMC for R_0 total
    MCMC for R_0 total

--- Model_super_spreading/4biii_MCMC_SS_Invesitage_R0_total.R
+++ Model_super_spreading/4biii_MCMC_SS_Invesitage_R0_total.R
@@ -15,6 +15,7 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
   #Set up
   alpha_vec <- vector('numeric', n); beta_vec <- vector('numeric', n)
   gamma_vec <- vector('numeric', n); r0_total_vec <- vector('numeric', n)
+  count_accept1 = 0; count_accept2 = 0; count_accept3 = 0;
   
   #Initialise parameters
   r0_total_vec[1] <- x0
@@ -40,16 +41,13 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
     gamma_vec[1] <- x0
   }
   
-  count_accept1 = 0; count_reject1 = 0; count_accept2 = 0
-  count_reject2 = 0; count_accept3 = 0; count_reject3 = 0
-  
   # #MCMC chain
   for(i in 2:n) {
     
     #******************************************************
     #alpha
     if (flag_infer == 'alpha') {
-      alpha_vec[1] <- alphaX; beta_vec[1] <- betaX; gamma_vec[1] <- x0
+      #alpha_vec[1] <- alphaX; beta_vec[1] <- betaX; gamma_vec[1] <- x0
       
       alpha_dash <- alpha_vec[i-1] + rnorm(1, sd = sigma)
       #cat("alpha dash: ", alpha_dash, "\n")
@@ -61,8 +59,8 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
       #log alpha
       logl_new = log_like_ss_lse(data, alpha_dash, beta_vec[i-1], gamma_vec[i-1])
       logl_prev = log_like_ss_lse(data, alpha_vec[i-1], beta_vec[i-1], gamma_vec[i-1])
-      prior1 = dgamma(alpha_dash, shape = 1, scale = 1, log = TRUE)
-      prior2 = dgamma(alpha_vec[i-1], shape = 1, scale = 1, log = TRUE)
+      #prior1 = dgamma(alpha_dash, shape = 1, scale = 1, log = TRUE)
+      #prior2 = dgamma(alpha_vec[i-1], shape = 1, scale = 1, log = TRUE)
       log_accept_prob = logl_new - logl_prev #+ prior1 - prior2
       
       if(!(is.na(log_accept_prob)) && log(runif(1)) < log_accept_prob) {
@@ -70,7 +68,6 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
         count_accept1 = count_accept1 + 1
       } else {
         alpha_vec[i] <- alpha_vec[i-1]
-        count_reject1 = count_reject1 + 1
       }
     } else {
       alpha_vec[i] <- alpha_vec[i-1]
@@ -84,10 +81,10 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
       beta_dash = abs(beta_dash)
     }
 
-    logl_new = log_like_ss_lse(data, alpha_vec[i], beta_dash, gamma_vec[i-1])
-    logl_prev = log_like_ss_lse(data, alpha_vec[i], beta_vec[i-1], gamma_vec[i-1])
-    prior1 = dgamma(beta_dash, shape = 1, scale = 1, log = TRUE)
-    prior2 = dgamma(beta_vec[i-1], shape = 1, scale = 1, log = TRUE)
+    logl_new = log_like_ss_lse(data, alpha_vec[i-1], beta_dash, gamma_vec[i-1])
+    logl_prev = log_like_ss_lse(data, alpha_vec[i-1], beta_vec[i-1], gamma_vec[i-1])
+    #prior1 = dgamma(beta_dash, shape = 1, scale = 1, log = TRUE)
+    #prior2 = dgamma(beta_vec[i-1], shape = 1, scale = 1, log = TRUE)
     log_accept_prob = logl_new - logl_prev #+ prior1 - prior2
 
     if (!(is.na(log_accept_prob)) &&
@@ -96,7 +93,6 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
       count_accept2 = count_accept2 + 1
     } else {
       beta_vec[i] <- beta_vec[i - 1]
-      count_reject2 = count_reject2 + 1
     }
     } else {
       beta_vec[i] <- beta_vec[i - 1]
@@ -123,7 +119,6 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
       count_accept3 = count_accept3 + 1
     } else {
       gamma_vec[i] <- gamma_vec[i-1]
-      count_reject3 = count_reject3 + 1
     }
     } else {
       gamma_vec[i] <- gamma_vec[i - 1]
@@ -134,27 +129,25 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
   }
   #Final stats
   #alpha
-  total_iters1 = count_accept1 + count_reject1
-  accept_rate1 = 100*(count_accept1/(count_accept1+count_reject1))
-  num_samples1 = count_accept1
-  print("Acceptance rate1 = ")
-  print(accept_rate1)
-  
+  accept_rate1 = 100*count_accept1/n
   #beta
-  total_iters2 = count_accept2 + count_reject2
-  accept_rate2 = 100*(count_accept2/(count_accept2+count_reject2))
-  num_samples2 = count_accept2
-  print("Acceptance rate2 = ")
-  print(accept_rate2)
+  accept_rate2 = 100*count_accept2/n
   
   #gamma
-  total_iters3 = count_accept3 + count_reject3
-  accept_rate3 = 100*(count_accept3/(count_accept3+count_reject3))
-  num_samples3 = count_accept3
-  print("Acceptance rate3 = ")
-  print(accept_rate3)
+  accept_rate3 = 100*count_accept3/n
   
-  #Print table of results 
+  #Results - Create dataframe
+  df_results <- data.frame(
+    accept_rate_alpha = accept_rate1, #unlist(list_accept_rate1),
+    n_accepted_a = count_accept,
+    accept_rate_beta = accept_rate2, #unlist(list_accept_rate2),
+    n_accepted_b = count_accept2, 
+    accept_rate_gamma = accept_rate3,
+    n_accepted_g = count_accept3) #unlist(list_accept_rate3))
+  
+    #time_taken = unlist(list_time_taken))
+  
+  print(df_results)
   
   #Burn-in 
   #alpha_vec = alpha_vec[burn_in:n]
@@ -162,10 +155,7 @@ mcmc_ss_investigate_r0_total <- function(data, n, sigma, alphaX, betaX, gammaX, 
   #gamma_vec = gamma_vec[burn_in:n]
   
   #Return alpha, acceptance rate
-  return(list(alpha_vec, beta_vec, gamma_vec, r0_total_vec,
-              accept_rate1, num_samples1, sigma, 
-              accept_rate2, num_samples2, sigma, 
-              accept_rate3, num_samples3, sigma))
+  return(list(alpha_vec, beta_vec, gamma_vec, r0_total_vec))
 }
 
 #********
@@ -177,9 +167,9 @@ shape_gamma = 6
 scale_gamma = 1 
 
 #params
-flag_infer = 'gamma'
-alphaX = 2 #2 #Without ss event, ~r0.
-betaX = 0.05 #0.05
+flag_infer = 'alpha'
+alphaX = 0.8 #2 #0.9 #2 #2 #Without ss event, ~r0.
+betaX = 0.2 #0.05 #0.2 #0.05 #0.05
 gammaX = 10
 true_tot_r0 = alphaX + betaX*gammaX
 
@@ -188,7 +178,7 @@ sim_data = simulate_branching_ss(num_days, shape_gamma, scale_gamma, alphaX, bet
 plot.ts(sim_data, ylab = 'Daily Infections count', main = 'Daily Infections count')
 
 #Time 
-n = 5000
+n = 50000
 start_time = Sys.time()
 print('Start time:')
 print(start_time)
@@ -212,36 +202,37 @@ gamma_mcmc = unlist(gamma_mcmc)
 r0_total_mcmc = mcmc_params_ad[4]
 r0_total_mcmc = unlist(r0_total_mcmc)
 
-#Plot
+#Plots
+#alpha
 plot.ts(alpha_mcmc, ylab = 'alpha', main = paste("MCMC Super spreading model, simulated alpha = ", alphaX))
-plot.ts(beta_mcmc, ylab = 'beta', main = paste("MCMC Super spreading model, simulated beta = ", betaX))
-plot.ts(gamma_mcmc,  ylab = 'gamma', main = paste("MCMC Super spreading model, simulated gamma = ", gammaX))
-plot.ts(r0_total_mcmc,  ylab = 'r0', main = paste("R0 total - MCMC Super spreading model, true total r0 = ", true_tot_r0))
-
 #alpha mean
 alpha_mean = cumsum(alpha_mcmc)/seq_along(alpha_mcmc)
 plot2 = plot(seq_along(alpha_mean), alpha_mean, xlab = 'Time', ylab = 'alpha', main = paste("Mean of alpha MCMC chain, True alpha = ",alphaX))
 print(plot2)
 
+#beta
+plot.ts(beta_mcmc, ylab = 'beta', main = paste("MCMC Super spreading model, simulated beta = ", betaX))
 #beta mean
 beta_mean = cumsum(beta_mcmc)/seq_along(beta_mcmc)
 plot2 = plot(seq_along(beta_mean), beta_mean, xlab = 'Time', ylab = 'beta', main = paste("Mean of beta MCMC chain, True beta = ",betaX))
 print(plot2)
 
+#gamma
+plot.ts(gamma_mcmc,  ylab = 'gamma', main = paste("MCMC Super spreading model, simulated gamma = ", gammaX))
 #gamma Mean
 gamma_mean = cumsum(gamma_mcmc)/seq_along(gamma_mcmc)
 plot2 = plot(seq_along(gamma_mean), gamma_mean, xlab = 'Time', ylab = 'gamma', main = paste("Mean of gamma MCMC chain, True gamma = ",gammaX))
 print(plot2)
 
+#r0
+plot.ts(r0_total_mcmc,  ylab = 'r0', main = paste("R0 total - MCMC Super spreading model, true total r0 = ", true_tot_r0))
 #r0 mean
 r0_tot_mean = cumsum(r0_total_mcmc)/seq_along(r0_total_mcmc)
 plot2 = plot(seq_along(r0_tot_mean), r0_tot_mean, xlab = 'Time', ylab = 'r0 total', main = paste("Mean of R0 total MCMC chain, True R0 total = ", true_tot_r0))
 print(plot2)
 
-#Histograms
-#Hist I
+#r0 hist
 hist(r0_total_mcmc, prob = TRUE, breaks = 80,main = paste("Histogram of R0_total MCMC samples, True R0 total = ", true_tot_r0))
-
 #Hist - density
 hist2 <- hist(r0_total_mcmc, breaks = 80)
 hist2$counts <- hist2$counts/sum(hist2$counts)
