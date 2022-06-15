41283aea990396e59bf35425104d3e518048a4c8
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2022-06-15 18:58:30 +0100

    PLOT FUNCTION refactored

--- plot_functions.R
+++ plot_functions.R
@@ -6,14 +6,10 @@
 #* FUNCTION TO PLOT 4x4 DASHBOARD OF MCMC RESULTS FOR SUPER SPREADING MODELs
 #* 
 #******************************************
-PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
-                           mcmc_inputs = list(n_mcmc = n_mcmc,
-                                              model_params = model_params,
-                                              mod_par_names = c('alpha', 'beta', 'gamma'),
-                                              sigma = sigma,
-                                              model_typeX = 'SSE',
-                                              total_time = 0, seed_count = 1,
-                                              x0 = 1),
+PLOT_MCMC_GRID <- function(sim_data, mcmc_output, 
+                           mcmc_plot_inputs = list(n_mcmc = n_mcmc, model_params = model_params, mod_par_names = c('a', 'b', 'c'),
+                                                   sigma = sigma, model_typeX = 'SSI', TYPEX = 'Individuals',
+                                                   seed_count = seed_count, x0 = 1),
                            priors_list = list(a_prior = c(1, 0), b_prior = c(10, 1/100), b_prior_exp = c(1,0),
                                               c_prior = c(10, 1), c_prior_exp = c(0.1,0)),
                            FLAGS_LIST = list(DATA_AUG = TRUE, BC_TRANSFORM = TRUE,
@@ -38,20 +34,20 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
   
   #m1
   m1_mean = cumsum(m1_mcmc)/seq_along(m1_mcmc)
-  print(mcmc_inputs$model_params$m1[[1]])
+  print(mcmc_plot_inputs$model_params$m1[[1]])
   print(max(m1_mcmc))
-  a_lim =  max(mcmc_inputs$model_params$m1[[1]], max(m1_mcmc))
-  a_lim2 =  max(mcmc_inputs$model_params$m1[[1]], m1_mean)
+  a_lim =  max(mcmc_plot_inputs$model_params$m1[[1]], max(m1_mcmc))
+  a_lim2 =  max(mcmc_plot_inputs$model_params$m1[[1]], m1_mean)
   
   #m2
   m2_mean = cumsum(m2_mcmc)/seq_along(m2_mcmc)
-  b_lim = max(mcmc_inputs$model_params$m2[[1]], max(m2_mcmc))
-  b_lim2 = max(mcmc_inputs$model_params$m2[[1]], m2_mean)
+  b_lim = max(mcmc_plot_inputs$model_params$m2[[1]], max(m2_mcmc))
+  b_lim2 = max(mcmc_plot_inputs$model_params$m2[[1]], m2_mean)
   
   #m3
   m3_mean = cumsum(m3_mcmc)/seq_along(m3_mcmc)
-  m3_lim =  max(mcmc_inputs$model_params$m3[[1]], max(m3_mcmc))
-  m3_lim2 =  max(mcmc_inputs$model_params$m3[[1]], m3_mean) 
+  m3_lim =  max(mcmc_plot_inputs$model_params$m3[[1]], max(m3_mcmc))
+  m3_lim2 =  max(mcmc_plot_inputs$model_params$m3[[1]], m3_mean) 
   
   #Priors
   if (FLAGS_LIST$B_PRIOR_GAMMA) {
@@ -72,74 +68,78 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
   #* Plots *
   
   #i.Infections
-  if(!FLAGS_LIST$DATA_AUG) inf_tite = paste0(mcmc_inputs$seed_count, ', ', mcmc_inputs$model_typeX, " Data, r0 = ", true_r0) # 'Day Infts, '
-  else inf_tite = paste0(mcmc_inputs$seed_count, ', ', mcmc_inputs$model_typeX, " Data, r0 = ", true_r0, ", + Data Aug")
+  if(!FLAGS_LIST$DATA_AUG) inf_tite = paste0(mcmc_plot_inputs$seed_count, ', ', mcmc_plot_inputs$model_typeX, " Data, r0 = ", true_r0) # 'Day Infts, '
+  else inf_tite = paste0(mcmc_plot_inputs$seed_count, ', ', mcmc_plot_inputs$model_typeX, " Data, r0 = ", true_r0, ", + Data Aug")
   
   #i.Infections
-  plot.ts(sim_data, xlab = 'Time', ylab = 'Daily Infections count',
+  sim_dataX = non_ss + ss
+  plot.ts(sim_dataX, xlab = 'Time', ylab = 'Daily Infections count',
           main = inf_tite, 
           cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
   
   #ii. MCMC Trace Plots 
-  plot.ts(m1_mcmc, ylab = mcmc_inputs$mod_par_names[1], ylim=c(0, a_lim),
-          main = paste("MCMC", mcmc_inputs$model_typeX, ":", mcmc_inputs$mod_par_names[1], "prior:", m1_prior),
+  plot.ts(m1_mcmc, ylab = mcmc_plot_inputs$mod_par_names[1], ylim=c(0, a_lim),
+          main = paste("MCMC", mcmc_plot_inputs$model_typeX, ":", mcmc_plot_inputs$mod_par_names[1], "prior:", m1_prior),
           cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(h = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2) #True = green
+  abline(h = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2) #True = green
   
   plot.ts(m2_mcmc, ylab = 'm2', ylim=c(0, b_lim), 
-          main = paste("MCMC", mcmc_inputs$model_typeX, ":", mcmc_inputs$mod_par_names[2], "prior:", m2_prior),
+          main = paste("MCMC", mcmc_plot_inputs$model_typeX, ":", mcmc_plot_inputs$mod_par_names[2], "prior:", m2_prior),
           cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(h = mcmc_inputs$model_params$m2[[1]], col = 'blue', lwd = 2) #True = green
+  abline(h = mcmc_plot_inputs$model_params$m2[[1]], col = 'blue', lwd = 2) #True = green
   
   plot.ts(m3_mcmc,  ylab = 'm3', ylim=c(0,m3_lim),
-          main = paste("MCMC", mcmc_inputs$model_typeX, ":", mcmc_inputs$mod_par_names[3], "prior:", m3_prior),
+          main = paste("MCMC", mcmc_plot_inputs$model_typeX, ":", mcmc_plot_inputs$mod_par_names[3], "prior:", m3_prior),
           cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(h = mcmc_inputs$model_params$m3[[1]], col = 'green', lwd = 2) #True = green
+  abline(h = mcmc_plot_inputs$model_params$m3[[1]], col = 'green', lwd = 2) #True = green
   
   #plot.ts(r0_mcmc,  ylab = 'r0', main = paste("MCMC SS Events, true r0 = ", r0_true))
   
+  #ib. DATA - REGULAR SPREADING (row II)
+  non_ss = sim_data[[1]]
+  plot.ts(non_ss, ylab = 'Daily Infections count', main = 'Non Super-Spreading')
+
   #iii. Cumulative mean plots
   #r0 Mean
-  plot(seq_along(r0_mean), r0_mean,
-       ylim=c(0, r0_lim),
-       xlab = 'Time', ylab = 'R0', main = paste("R0 MCMC Mean, True R0 = ", true_r0),
-       cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  #print(plot2)
-  abline(h = true_r0, col = 'orange', lwd = 2)
+  # plot(seq_along(r0_mean), r0_mean,
+  #      ylim=c(0, r0_lim),
+  #      xlab = 'Time', ylab = 'R0', main = paste("R0 MCMC Mean, True R0 = ", true_r0),
+  #      cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+  # #print(plot2)
+  # abline(h = true_r0, col = 'orange', lwd = 2)
   
   #m1 mean
   plot(seq_along(m1_mean), m1_mean,
        ylim=c(0, a_lim),
-       xlab = 'Time', ylab =  mcmc_inputs$mod_par_names[1],
-       main = paste(mcmc_inputs$mod_par_names[1], "MCMC mean, True", mcmc_inputs$mod_par_names[1], "=", mcmc_inputs$model_params$m1[[1]]),
+       xlab = 'Time', ylab =  mcmc_plot_inputs$mod_par_names[1],
+       main = paste(mcmc_plot_inputs$mod_par_names[1], "MCMC mean, True", mcmc_plot_inputs$mod_par_names[1], "=", mcmc_plot_inputs$model_params$m1[[1]]),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
   #print(plot2)
-  abline(h = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
+  abline(h = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
   
   #m2 mean
   plot(seq_along(m2_mean), m2_mean,
        ylim=c(0, b_lim),
        xlab = 'Time', ylab = 'm2',
-       main = paste(mcmc_inputs$mod_par_names[2], "MCMC mean, True", mcmc_inputs$mod_par_names[2], "=", mcmc_inputs$model_params$m2[[1]]),
+       main = paste(mcmc_plot_inputs$mod_par_names[2], "MCMC mean, True", mcmc_plot_inputs$mod_par_names[2], "=", mcmc_plot_inputs$model_params$m2[[1]]),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
   #print(plot2)
-  abline(h = mcmc_inputs$model_params$m2[[1]], col = 'blue', lwd = 2)
+  abline(h = mcmc_plot_inputs$model_params$m2[[1]], col = 'blue', lwd = 2)
   
   #m3 Mean
   plot(seq_along(m3_mean), m3_mean,
        xlab = 'Time', ylab = 'm3', 
-       main = paste(mcmc_inputs$mod_par_names[3], "MCMC mean, True", mcmc_inputs$mod_par_names[3], "=", mcmc_inputs$model_params$m3[[1]]),
+       main = paste(mcmc_plot_inputs$mod_par_names[3], "MCMC mean, True", mcmc_plot_inputs$mod_par_names[3], "=", mcmc_plot_inputs$model_params$m3[[1]]),
        ylim=c(0, m3_lim),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(h = mcmc_inputs$model_params$m3[[1]], col = 'green', lwd = 2)
+  abline(h = mcmc_plot_inputs$model_params$m3[[1]], col = 'green', lwd = 2)
+  
+  #****************
+  #ROW 3
+  ss = sim_data[[2]]
+  plot.ts(ss, ylab = 'Daily Infections count', main = paste0('Super-Spreading', mcmc_plot_inputs$TYPEX))
   
   #iv. Param Histograms (Plots 9,11,12)
-  hist(r0_mcmc, freq = FALSE, breaks = 100,
-       xlab = 'R0 total', #ylab = 'Density', 
-       main = paste('R0 total MCMC samples'),
-       xlim=c(0, r0_lim),
-       cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(v = true_r0, col = 'orange', lwd = 2)
   
   # #v. m2 vs m3
   # plot(m2_mcmc, m3_mcmc,
@@ -148,59 +148,67 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
   
   #Hist m1 
   hist(m1_mcmc, freq = FALSE, breaks = 100,
-       xlab = mcmc_inputs$mod_par_names[1], #ylab = 'Density', 
-       main = paste(mcmc_inputs$mod_par_names[1], ", True", mcmc_inputs$mod_par_names[1], "=", mcmc_inputs$model_params$m1[[1]]), 
+       xlab = mcmc_plot_inputs$mod_par_names[1], #ylab = 'Density', 
+       main = paste(mcmc_plot_inputs$mod_par_names[1], ", True", mcmc_plot_inputs$mod_par_names[1], "=", mcmc_plot_inputs$model_params$m1[[1]]), 
        xlim=c(0, a_lim),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(v = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
+  abline(v = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
   
   #Hist m2 
   hist(m2_mcmc, freq = FALSE, breaks = 100,
-       xlab = mcmc_inputs$mod_par_names[2], #ylab = 'Density', 
-       main = paste(mcmc_inputs$mod_par_names[2], ", True", mcmc_inputs$mod_par_names[2], "=", mcmc_inputs$model_params$m2[[1]]), 
+       xlab = mcmc_plot_inputs$mod_par_names[2], #ylab = 'Density', 
+       main = paste(mcmc_plot_inputs$mod_par_names[2], ", True", mcmc_plot_inputs$mod_par_names[2], "=", mcmc_plot_inputs$model_params$m2[[1]]), 
        xlim=c(0, b_lim),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(v = mcmc_inputs$model_params$m2[[1]], col = 'blue', lwd = 2)
+  abline(v = mcmc_plot_inputs$model_params$m2[[1]], col = 'blue', lwd = 2)
   
   #Hist m3 
   hist(m3_mcmc, freq = FALSE, breaks = 100,
-       xlab = mcmc_inputs$mod_par_names[3], #ylab = 'Density', 
-       main = paste(mcmc_inputs$mod_par_names[3], ", True", mcmc_inputs$mod_par_names[3], "=", mcmc_inputs$model_params$m3[[1]]),
+       xlab = mcmc_plot_inputs$mod_par_names[3], #ylab = 'Density', 
+       main = paste(mcmc_plot_inputs$mod_par_names[3], ", True", mcmc_plot_inputs$mod_par_names[3], "=", mcmc_plot_inputs$model_params$m3[[1]]),
        xlim=c(0, m3_lim),
        cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-  abline(v = mcmc_inputs$model_params$m3[[1]], col = 'green', lwd = 2)
+  abline(v = mcmc_plot_inputs$model_params$m3[[1]], col = 'green', lwd = 2)
   
   #Final Mean Stats
-  data_10_pc = 0.5*mcmc_inputs$n_mcmc #50%
-  m1_mean_tail = round(mean(m1_mcmc[mcmc_inputs$n_mcmc - data_10_pc:mcmc_inputs$n_mcmc]), 2) 
-  m2_mean_tail = round(mean(m2_mcmc[mcmc_inputs$n_mcmc - data_10_pc:mcmc_inputs$n_mcmc]), 2)
-  m3_mean_tail = round(mean(m3_mcmc[mcmc_inputs$n_mcmc - data_10_pc:mcmc_inputs$n_mcmc]), 2)
-  m4_mean_tail = round(mean(r0_mcmc[mcmc_inputs$n_mcmc - data_10_pc:mcmc_inputs$n_mcmc]), 2)
+  data_10_pc = 0.5*mcmc_plot_inputs$n_mcmc #50%
+  m1_mean_tail = round(mean(m1_mcmc[mcmc_plot_inputs$n_mcmc - data_10_pc:mcmc_plot_inputs$n_mcmc]), 2) 
+  m2_mean_tail = round(mean(m2_mcmc[mcmc_plot_inputs$n_mcmc - data_10_pc:mcmc_plot_inputs$n_mcmc]), 2)
+  m3_mean_tail = round(mean(m3_mcmc[mcmc_plot_inputs$n_mcmc - data_10_pc:mcmc_plot_inputs$n_mcmc]), 2)
+  m4_mean_tail = round(mean(r0_mcmc[mcmc_plot_inputs$n_mcmc - data_10_pc:mcmc_plot_inputs$n_mcmc]), 2)
+  
+  #ROW 4
+  hist(r0_mcmc, freq = FALSE, breaks = 100,
+       xlab = 'R0 total', #ylab = 'Density', 
+       main = paste('R0 total MCMC samples'),
+       xlim=c(0, r0_lim),
+       cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+  abline(v = true_r0, col = 'orange', lwd = 2)
   
   #FLAGS_LIST$JOINT distrbutions
   if (FLAGS_LIST$JOINT){
     
     #v. r0 vs m2
-    plot(m2_mcmc, r0_mcmc,
-         xlab = mcmc_inputs$mod_par_names[2], ylab = 'R0', main = paste(mcmc_inputs$mod_par_names[2], 'vs R0'),
-         cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5,
-         cex.main = 0.5)
+    # plot(m2_mcmc, r0_mcmc,
+    #      xlab = mcmc_plot_inputs$mod_par_names[2], ylab = 'R0', main = paste(mcmc_plot_inputs$mod_par_names[2], 'vs R0'),
+    #      cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5,
+    #      cex.main = 0.5)
     
     #v. m1 vs m2
     plot(m1_mcmc, m2_mcmc,
-         xlab = mcmc_inputs$mod_par_names[1], ylab = mcmc_inputs$mod_par_names[2], main = paste(mcmc_inputs$mod_par_names[1], 'vs', mcmc_inputs$mod_par_names[2]),
+         xlab = mcmc_plot_inputs$mod_par_names[1], ylab = mcmc_plot_inputs$mod_par_names[2], main = paste(mcmc_plot_inputs$mod_par_names[1], 'vs', mcmc_plot_inputs$mod_par_names[2]),
          cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5,
          cex = 0.5)
     
     #v. m1 vs m3
     plot(m1_mcmc, m3_mcmc,
-         xlab = mcmc_inputs$mod_par_names[1], ylab = mcmc_inputs$mod_par_names[3], main = paste(mcmc_inputs$mod_par_names[1], 'vs', mcmc_inputs$mod_par_names[3]),
+         xlab = mcmc_plot_inputs$mod_par_names[1], ylab = mcmc_plot_inputs$mod_par_names[3], main = paste(mcmc_plot_inputs$mod_par_names[1], 'vs', mcmc_plot_inputs$mod_par_names[3]),
          cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5,
          cex = 0.5)
     
     #v. m2 vs m3
     plot(m2_mcmc, m3_mcmc,
-         xlab = mcmc_inputs$mod_par_names[2], ylab = mcmc_inputs$mod_par_names[3], main = paste(mcmc_inputs$mod_par_names[2], 'vs', mcmc_inputs$mod_par_names[3]),
+         xlab = mcmc_plot_inputs$mod_par_names[2], ylab = mcmc_plot_inputs$mod_par_names[3], main = paste(mcmc_plot_inputs$mod_par_names[2], 'vs', mcmc_plot_inputs$mod_par_names[3]),
          cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5,
          cex = 0.5)
   }
@@ -214,11 +222,11 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
     
     #HIST m1 
     hist(m1_mcmc, freq = FALSE, breaks = 100,
-         xlab = mcmc_inputs$mod_par_names[1], #ylab = 'Density', 
-         main = paste("m1, True m1 = ", mcmc_inputs$model_params$m1[[1]]), 
+         xlab = mcmc_plot_inputs$mod_par_names[1], #ylab = 'Density', 
+         main = paste("m1, True m1 = ", mcmc_plot_inputs$model_params$m1[[1]]), 
          xlim=c(0, a_lim),
          cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-    abline(v = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
+    abline(v = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
     
     #m1 SSE
     ind_b = which(m2_mcmc > 0)
@@ -230,7 +238,7 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
          main = "m1_sse", 
          xlim=c(0, a_lim),
          cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-    abline(v = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
+    abline(v = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
     
     #m1 BASE
     ind_b = which(m2_mcmc == 0)
@@ -244,18 +252,18 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
            main = "m1_base",
            xlim=c(0, a_lim),
            cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-      abline(v = mcmc_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
+      abline(v = mcmc_plot_inputs$model_params$m1[[1]], col = 'red', lwd = 2)
     }
     
     #RESULTS DF
     df_results <- data.frame(
-      rep = mcmc_inputs$seed_count,
-      n_mcmc = mcmc_inputs$n_mcmc,
-      m1 = mcmc_inputs$model_params$m1[[1]],
+      rep = mcmc_plot_inputs$seed_count,
+      n_mcmc = mcmc_plot_inputs$n_mcmc,
+      m1 = mcmc_plot_inputs$model_params$m1[[1]],
       a_mc = a_mcmc_mean,
-      m2 = mcmc_inputs$model_params$m2[[1]],
+      m2 = mcmc_plot_inputs$model_params$m2[[1]],
       b_mc = b_mcmc_mean,
-      m3 = mcmc_inputs$model_params$m3[[1]],
+      m3 = mcmc_plot_inputs$model_params$m3[[1]],
       c_mc = c_mcmc_mean,
       R0 = true_r0, 
       R0_mc = r0_mcmc_mean,
@@ -275,18 +283,18 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
       m2_pc0 = mcmc_output[[18]],
       m2_pc_non_0 = 1- mcmc_output[[18]],
       bf = mcmc_output[[19]])
-    #tot_time = mcmc_inputs$total_time)
+    #tot_time = mcmc_plot_inputs$total_time)
     
   } else if (FLAGS_LIST$DATA_AUG) {
     
     df_results <- data.frame(
-      rep = mcmc_inputs$seed_count,
-      n_mcmc = mcmc_inputs$n_mcmc,
-      m1 = mcmc_inputs$model_params$m1[[1]],
+      rep = mcmc_plot_inputs$seed_count,
+      n_mcmc = mcmc_plot_inputs$n_mcmc,
+      m1 = mcmc_plot_inputs$model_params$m1[[1]],
       m1_mc = m1_mean_tail,
-      m2 = mcmc_inputs$model_params$m2[[1]],
+      m2 = mcmc_plot_inputs$model_params$m2[[1]],
       m2_mc = m2_mean_tail,
-      m3 = mcmc_inputs$model_params$m3[[1]],
+      m3 = mcmc_plot_inputs$model_params$m3[[1]],
       m3_mc = m3_mean_tail,
       R0 = true_r0, 
       R0_mc = m4_mean_tail,
@@ -303,13 +311,13 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
     
   } else {
     df_results <- data.frame(
-      rep = mcmc_inputs$seed_count,
-      n_mcmc = mcmc_inputs$n_mcmc,
-      m1 = mcmc_inputs$model_params$m1[[1]],
+      rep = mcmc_plot_inputs$seed_count,
+      n_mcmc = mcmc_plot_inputs$n_mcmc,
+      m1 = mcmc_plot_inputs$model_params$m1[[1]],
       m1_mc = a_mcmc_mean,
-      m2 = mcmc_inputs$model_params$m2[[1]],
+      m2 = mcmc_plot_inputs$model_params$m2[[1]],
       m2_mc = b_mcmc_mean,
-      m3 = mcmc_inputs$model_params$m3[[1]],
+      m3 = mcmc_plot_inputs$model_params$m3[[1]],
       m3_mc = c_mcmc_mean,
       R0 = true_r0, 
       R0_mc = r0_mcmc_mean,
@@ -321,9 +329,11 @@ PLOT_MCMC_GRID <- function(sim_data, mcmc_output,
       b_es = effectiveSize(as.mcmc(b)),
       c_es = effectiveSize(as.mcmc(c)),
       d_es = effectiveSize(as.mcmc(d)),
-      tot_time = mcmc_inputs$total_time)
+      tot_time = mcmc_output$time_elap)
   }
   
   print(df_results)
+  
+  return(df_results)
   
 }
